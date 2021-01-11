1f1479a18a05c342ad66dc8d36f478d0dc3a68d1
dima <example@mail.com>
2021-01-11 09:25:09 +0300

    prompt

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 "use strict";
 let cat = 'toys';
 console.log(`mine ${cat}`);
+const numberOfFilms = +prompt("сколько фильмо вы смотрели?",'');
+const personalMovieDB = {
+    count:numberOfFilms,
+    movies:{},
+    actors:{},
+     genres:[],
+     privat:false
+}
+const a = prompt("какой последний фильм вы смотрели?",''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt("какой последний фильм вы смотрели?",''),
+      d = prompt('На сколько оцените его?','');
+      personalMovieDB.movies[a]=b;
+      personalMovieDB.movies[c]=d;
+      console.log(personalMovieDB);
